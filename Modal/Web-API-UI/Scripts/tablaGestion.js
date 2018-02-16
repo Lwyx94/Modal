cc5574619d8cd7d666580a53f5e893ee75000304
Lwyx94 <luisgutierrezdiaz_@hotmail.com>
2018-02-16 12:44:09 +0100

    Que le den por culo
    Falta cambiar el tamaño de la tabla

--- Modal/Web-API-UI/Scripts/tablaGestion.js
+++ Modal/Web-API-UI/Scripts/tablaGestion.js
@@ -4,6 +4,7 @@ var dataTable;
 var tbody;
 var tableHead;
 var filasBorradas = 0;
+var arraycosas = ["Saab", "Volvo", "BMW"];
 
 function displayResult() {
     //document.getElementById("table1").insertRow(-1).innerHTML = '<td>Producto</td><td>Stock</td><td>Descripción</td><td>Cantidad</td><td>Precio Total</td>';
@@ -39,12 +40,31 @@ function displayResult() {
     tr.appendChild(td4);
     tr.appendChild(td5);
 
-    //td0.innerHTML = i + 1;
-    td0.innerHTML = "asdasda"+fila;//meter el select de productos
-    td1.innerHTML = "asdasda" + fila; //stock
-    td2.innerHTML = "asdasda" + fila;//descrupcion
-    td3.innerHTML = "asdasda" + fila; //cantidad
-    td4.innerHTML = "asdasda" + fila;//precio
+    td0.appendChild(document.createElement("SELECT"));
+
+    for (var i = 0; i < arraycosas.length; i++)
+    {
+        var option = document.createElement("option");
+        option.value = arraycosas[i];
+        option.text = arraycosas[i];
+        td0.firstChild.appendChild(option);
+    }
+
+    td0.firstChild.addEventListener("change", function ()
+    {
+        var x = document.createElement("INPUT");
+        x.setAttribute("type", "number");
+
+        td1.innerHTML = "stock" + td0.firstChild.value; //stock de la api
+        td2.innerHTML = "descripcion" + td0.firstChild.value;//descrupcion
+        if (td3.firstChild == null)
+        {
+            td3.appendChild(x); //cantidad
+        }
+        td4.innerHTML = "precio" + td0.firstChild.value;//precio
+    });
+
+    
     td5.appendChild(btnDelete);
 
     //AÑADE A CADA BOTON ELIMINAR UN LISTENER PARA EL METODO
@@ -59,23 +79,21 @@ function displayResult() {
 
 function eliminarFila() {
     if (document.getElementById("table1").childElementCount - 1 <= this.name) {
-        document.getElementById("table1").deleteRow(parseInt(this.name) - filasBorradas);
+        document.getElementById("table1").rows.item(parseInt(this.name)).style.display = 'none';
     }
 
     else
     {
-        document.getElementById("table1").deleteRow(this.name);
+        document.getElementById("table1").rows.item(parseInt(this.name)).style.display = 'none';
     }
-    filasBorradas++;
-    fila--;
 }
 
 function borrarTabla() {
 
-    while (dataTable.firstChild) {
-        
-        dataTable.removeChild(dataTable.firstChild);
+    for (var i = 1; i < document.getElementById("table1").rows.length; i++)
+    {
+        document.getElementById("table1").rows.item(i).style.display = 'none';
     }
-    fila = 1;
+
 }
 
